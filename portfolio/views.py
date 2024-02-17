5c76a4250e33b919925bb09f51b54cc91a42269e
motomario <marius.kaz@icloud.com>
2024-02-17 15:33:11 +0200

    obd scanner portfolio done

--- portfolio/views.py
+++ portfolio/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index_view(request):
     return render(request, 'portfolio/index.html')
+
+def scrapesheet_plotly(request):
+    return render(request, 'portfolio/scrapesheet_plotly.html')
+
+def scrapesheet_seaborn(request):
+    return render(request, 'portfolio/scrapesheet_seaborn.html')
